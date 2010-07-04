1e91a6739ec326e65a6c1bcc325dfca0d6c963c6
John Nunemaker <nunemaker@gmail.com>
2010-07-04 16:32:44 -0400

    More attribute related tests and such.

--- lib/toy/attributes.rb
+++ lib/toy/attributes.rb
@@ -23,39 +23,41 @@ module Toy
       end
     end
 
-    def attributes
-      @attributes ||= {}
-    end
+    module InstanceMethods
+      def attributes
+        @attributes ||= {}
+      end
 
-    def respond_to?(*args)
-      self.class.define_attribute_methods
-      super
-    end
-
-    def method_missing(method, *args, &block)
-      if !self.class.attribute_methods_generated?
+      def respond_to?(*args)
         self.class.define_attribute_methods
-        send(method, *args, &block)
-      else
         super
       end
+
+      def method_missing(method, *args, &block)
+        if !self.class.attribute_methods_generated?
+          self.class.define_attribute_methods
+          send(method, *args, &block)
+        else
+          super
+        end
+      end
+
+      protected
+        def attribute_method?(name)
+          self.class.attribute?(name)
+        end
+
+        def attribute(name)
+          attributes[name]
+        end
+
+        def attribute=(name, value)
+          attributes[name] = value
+        end
+
+        def attribute?(name)
+          attribute(name).present?
+        end
     end
-
-    protected
-      def attribute_method?(name)
-        self.class.attribute?(name)
-      end
-
-      def attribute(name)
-        attributes[name]
-      end
-
-      def attribute=(name, value)
-        attributes[name] = value
-      end
-
-      def attribute?(name)
-        attribute(name).present?
-      end
   end
 end
--- spec/toy/attributes_spec.rb
+++ spec/toy/attributes_spec.rb
@@ -1,6 +1,12 @@
 require 'helper'
 
 describe Toy::Attributes do
+  describe "including" do
+    it "adds #attributes defaulting to empty hash" do
+      Model().new.attributes.should == {}
+    end
+  end
+
   describe "declaring an attribute" do
     before do
       @model = Model() do
@@ -22,12 +28,19 @@ describe Toy::Attributes do
       record.name = 'John'
       record.name.should == 'John'
     end
-    
+
     it "adds query-ers" do
       record = model.new
       record.name?.should be_false
       record.name = 'John'
       record.name?.should be_true
+    end
+
+    it "knows if it responds to attribute method" do
+      record = model.new
+      record.respond_to?(:name)
+      record.respond_to?(:name=)
+      record.respond_to?(:name?)
     end
   end
 end
