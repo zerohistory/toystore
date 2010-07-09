9bc6abed97d018ca9189d9187ec40a81fd438490
John Nunemaker <nunemaker@gmail.com>
2010-07-09 17:41:23 -0400

    Hooked up validations.

--- lib/toy/validations.rb
+++ lib/toy/validations.rb
@@ -2,5 +2,20 @@ module Toy
   module Validations
     extend ActiveSupport::Concern
     include ActiveModel::Validations
+
+    included do
+      extend ActiveModel::Callbacks
+      define_model_callbacks :validation
+    end
+
+    module InstanceMethods
+      def valid?
+        run_callbacks(:validation) { super }
+      end
+
+      def save
+        valid? ? super : false
+      end
+    end
   end
 end
--- spec/toy/validations_spec.rb
+++ spec/toy/validations_spec.rb
@@ -1,13 +1,85 @@
 require 'helper'
 
 describe Toy::Validations do
-  it "should include ActiveModel validations" do
-    @model = Model('Foo') do
+  before do
+    @model = Model('User') do
       attribute :name
       validates_presence_of :name
+
+      [:before_validation, :after_validation].each do |callback|
+        callback_method = "#{callback}_callback"
+        send(callback, callback_method)
+        define_method(callback_method) { history << callback.to_sym }
+      end
+
+      def history
+        @history ||= []
+      end
     end
-    
-    @model.new(:name => nil).should_not be_valid
-    @model.new(:name => 'something').should be_valid
+  end
+  let(:model) { @model }
+
+  it "runs validation callbacks around valid?" do
+    doc = model.new(:name => 'John')
+    doc.valid?
+    doc.history.should == [:before_validation, :after_validation]
+  end
+
+  describe "validate callback" do
+    before do
+      @model = Model('User') do
+        attribute :name
+        validate :name_not_john
+
+        private
+          def name_not_john
+            errors.add(:name, 'cannot be John') if name == 'John'
+          end
+      end
+    end
+    let(:model) { @model }
+
+    it "adds error if fails" do
+      doc = model.new(:name => 'John')
+      doc.valid?
+      doc.errors[:name].should include('cannot be John')
+    end
+
+    it "does not add error if all good" do
+      doc = model.new(:name => 'Frank')
+      doc.should be_valid
+    end
+  end
+
+  describe "#save" do
+    describe "with valid" do
+      before do
+        @doc = model.new(:name => 'John')
+        @result = @doc.save
+      end
+
+      it "returns true" do
+        @result.should be_true
+      end
+
+      it "persists the instance" do
+        @doc.should be_persisted
+      end
+    end
+
+    describe "with invalid" do
+      before do
+        @doc = model.new
+        @result = @doc.save
+      end
+
+      it "returns false" do
+        @result.should be_false
+      end
+
+      it "does not persist instance" do
+        @doc.should_not be_persisted
+      end
+    end
   end
 end
