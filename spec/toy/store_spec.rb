640a49f0ea119e3dbbd8e55299de406bb26821b8
John Nunemaker <nunemaker@gmail.com>
2010-07-23 17:06:48 -0400

    Made specs for to_key and to_param no longer pending for created objects.

--- spec/toy/store_spec.rb
+++ spec/toy/store_spec.rb
@@ -1,38 +1,41 @@
 require 'helper'
 
 describe Toy::Store do
+  before      { @model = Model('User') }
+  let(:model) { @model }
+  
   describe "including" do
     it "adds model naming" do
-      model_name = User.model_name
+      model_name = model.model_name
       model_name.should           == 'User'
       model_name.singular.should  == 'user'
       model_name.plural.should    == 'users'
     end
 
     it "adds to_model" do
-      user = User.new
+      user = model.new
       user.to_model.should == user
     end
 
     describe "#to_key" do
-      xit "returns [id] if persisted" do
-        user = User.create
+      it "returns [id] if persisted" do
+        user = model.create
         user.to_key.should == [user.id]
       end
 
       it "returns nil if not persisted" do
-        User.new.to_key.should be_nil
+        model.new.to_key.should be_nil
       end
     end
 
     describe "#to_param" do
-      xit "returns to_key if to_key present" do
-        user = User.create
-        user.to_param.should == user.to_key
+      it "returns key joined by - if to_key present" do
+        user = model.create
+        user.to_param.should == user.to_key.to_s
       end
 
       it "returns nil if to_key nil" do
-        User.new.to_param.should be_nil
+        model.new.to_param.should be_nil
       end
     end
   end
