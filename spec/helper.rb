53f9ccad34965d9dc98b85aa98e6bff5a7c12d5c
John Nunemaker <nunemaker@gmail.com>
2010-08-11 12:12:34 -0400

    Added log for tests and ignored it in git.

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,8 +1,12 @@
 require 'pathname'
+require 'logger'
 
 root_path   = Pathname(__FILE__).dirname.join('..').expand_path
 lib_path    = root_path.join('lib')
+log_path    = root_path.join('log')
 moneta_path = root_path.join('vendor', 'moneta', 'lib')
+
+log_path.mkpath
 
 $:.unshift(lib_path, moneta_path)
 
@@ -15,16 +19,18 @@ require 'moneta/file'
 require 'moneta/redis'
 require 'moneta/mongodb'
 
-LogBuddy.init
+Log = Logger.new(log_path.join('test.log'))
+
+LogBuddy.init :logger => Log
 
 FileStore  = Moneta::File.new(:path => 'testing')
 MongoStore = Moneta::MongoDB.new
 RedisStore = Moneta::Redis.new
 
 Spec::Runner.configure do |config|
+  config.include(Support::Constants)
+
   config.before(:each) do
     [FileStore, MongoStore, RedisStore].each(&:clear)
   end
-
-  config.include(Support::Constants)
 end
