33b680c8b9e7855e603e030586fed7b891f05cd9
John Nunemaker <nunemaker@gmail.com>
2010-08-19 11:10:27 -0400

    Added more docs to list with block/extensions.

--- lib/toy/lists.rb
+++ lib/toy/lists.rb
@@ -12,6 +12,19 @@ module Toy
       #   list :games, :dependent => true               # assumes Game
       #   list :active_games, Game                      # uses Game
       #   list :active_games, Game, :dependent => true  # uses Game
+      #
+      #   list :games do
+      #     def active
+      #       target.select { |t| t.active? }
+      #     end
+      #   end
+      #
+      #   module ActiveExtension
+      #     def active
+      #       target.select { |t| t.active? }
+      #     end
+      #   end
+      #   list :games, :extensions => [ActiveExtension]
       def list(name, *args, &block)
         List.new(self, name, *args, &block)
       end
