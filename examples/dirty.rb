d11897babf7e797e0d9a726f7cbc89d51ab4904b
John Nunemaker <nunemaker@gmail.com>
2010-08-20 11:27:45 -0400

    Added a dirty example.

---
+++ examples/dirty.rb
@@ -0,0 +1,30 @@
+require 'pathname'
+
+root_path   = Pathname(__FILE__).dirname.join('..').expand_path
+lib_path    = root_path.join('lib')
+moneta_path = root_path.join('vendor', 'moneta', 'lib')
+
+$:.unshift(lib_path, moneta_path)
+
+require 'toy'
+require 'moneta/redis'
+
+class User
+  include Toy::Store
+  store Moneta::Redis.new
+  attribute :name, String
+end
+
+user = User.new
+user.name = 'John'
+puts user.name_changed?
+puts user.changed?.inspect
+puts user.changed.inspect
+puts user.changes.inspect
+puts
+
+user.save # clears changes
+puts user.name_changed?
+puts user.changed?.inspect
+puts user.changed.inspect
+puts user.changes.inspect
