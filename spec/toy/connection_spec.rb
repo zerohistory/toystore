91de309bcd183fbb3475f62b00fcc17c6b7ab93f
John Nunemaker <nunemaker@gmail.com>
2010-08-26 14:05:35 -0400

    Use a simple dynamic class instead of constant stuff to test default store.

--- spec/toy/connection_spec.rb
+++ spec/toy/connection_spec.rb
@@ -7,45 +7,41 @@ describe Toy::Connection do
       Toy.store.should == RedisStore
     end
 
-    it "should use the default store, if present" do
-      remove_constants("Challenge")
-      class Challenge
-        include Toy::Store
-      end
-      
+    it "including Toy::Store should use the default store, if present" do
       Toy.store = MongoStore
-      Challenge.store.should == MongoStore
+      klass = Class.new { include Toy::Store }
+      klass.store.should == MongoStore
     end
   end
-  
+
   describe "logger" do
     it "should set the default logger" do
       logger = stub
       Toy.logger = logger
       Toy.logger.should == logger
     end
-    
+
     it "should be an instance of Logger if not set" do
       Toy.logger = nil
       Toy.logger.should be_instance_of(Logger)
     end
-    
+
     it "should use RAILS_DEFAULT_LOGGER if defined" do
       remove_constants("RAILS_DEFAULT_LOGGER")
       RAILS_DEFAULT_LOGGER = stub
-            
+
       Toy.logger = nil
       Toy.logger.should == RAILS_DEFAULT_LOGGER
-    end    
+    end
   end
-  
+
   describe "key_factory" do
     it "should set the default key_factory" do
       key_factory = stub
       Toy.key_factory = key_factory
       Toy.key_factory.should == key_factory
     end
-    
+
     it "should default to the UUIDKeyFactory" do
       Toy.key_factory = nil
       Toy.key_factory.should be_instance_of(Toy::Identity::UUIDKeyFactory)
