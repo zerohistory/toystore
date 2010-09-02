89002f07ab5472622d5108be1597e05566900436
John Nunemaker <nunemaker@gmail.com>
2010-09-02 16:18:40 -0400

    Switched to just default method instead of setting instance variable.

--- lib/toy/attribute.rb
+++ lib/toy/attribute.rb
@@ -1,21 +1,23 @@
 module Toy
   class Attribute
-    attr_reader :model, :name, :type
+    attr_reader :model, :name, :type, :options
 
     def initialize(model, name, type, options={})
       options.assert_valid_keys(:default)
-
-      @model, @name, @type = model, name.to_sym, type
-      @default = options[:default]
+      @model, @name, @type, @options = model, name.to_sym, type, options
       model.attributes[name] = self
     end
 
     def read(value)
-      type.from_store(value || @default) 
+      type.from_store(value || default) 
     end
 
     def write(value)
-      type.to_store(value || @default)
+      type.to_store(value || default)
+    end
+
+    def default
+      options[:default]
     end
 
     def eql?(other)
--- spec/toy/attribute_spec.rb
+++ spec/toy/attribute_spec.rb
@@ -21,6 +21,16 @@ describe Toy::Attribute do
     attribute.type.should == String
   end
 
+  describe "#default" do
+    it "defaults to nil" do
+      Toy::Attribute.new(User, :age, String).default.should be_nil
+    end
+
+    it "returns default if set" do
+      Toy::Attribute.new(User, :age, String, :default => 1).default.should == 1
+    end
+  end
+
   it "should write using the attribute type" do
     attribute.write(12).should == '12'
   end
