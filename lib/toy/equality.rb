a1ac0fc2aef1c0dce73be75ad3058fc803d630c3
John Nunemaker <nunemaker@gmail.com>
2010-09-07 15:13:51 -0400

    Make it so that a document can == and eql a reference to a document through the target.

--- lib/toy/equality.rb
+++ lib/toy/equality.rb
@@ -1,7 +1,11 @@
 module Toy
   module Equality
     def eql?(other)
-      self.class.eql?(other.class) && id == other.id
+      return true if self.class.eql?(other.class) && id == other.id
+      return true if other.respond_to?(:target) &&
+                     self.class.eql?(other.target.class) &&
+                     id == other.target.id
+      false
     end
     alias :== :eql?
   end
--- spec/toy/equality_spec.rb
+++ spec/toy/equality_spec.rb
@@ -1,20 +1,26 @@
 require 'helper'
 
 describe Toy::Dirty do
-  uses_constants('User', 'Person')
-  
+  uses_constants('User', 'Game', 'Person')
+
   describe "#eql?" do
     it "returns true if same class and id" do
       User.new(:id => 1).should eql(User.new(:id => 1))
     end
-    
+
     it "return false if different class" do
       User.new(:id => 1).should_not eql(Person.new(:id => 1))
     end
-    
+
     it "returns false if different id" do
       User.new(:id => 1).should_not eql(User.new(:id => 2))
     end
+
+    it "returns true if reference and target is same class and id" do
+      Game.reference(:user)
+      user = User.create
+      game = Game.create(:user => user)
+      user.should eql(game.user)
+    end
   end
-  
 end
