d0da3f05f22e93fab6a1bee7f8fc75e05f9e9d6a
John Nunemaker <nunemaker@gmail.com>
2010-09-08 14:43:32 -0400

    Added parent_reference shortcut for defining method to get at the parent_reference or check it's presence.

--- lib/toy/embedded_lists.rb
+++ lib/toy/embedded_lists.rb
@@ -7,6 +7,28 @@ module Toy
         @lists ||= {}
       end
 
+      def parent_reference_module
+        @parent_reference_module ||= begin
+          mod = Module.new
+          include mod
+          mod
+        end
+      end
+
+      def parent_reference(*names)
+        names.flatten.each do |name|
+          parent_reference_module.module_eval <<-CODE
+            def #{name}
+              parent_reference
+            end
+
+            def #{name}?
+              parent_reference.present?
+            end
+          CODE
+        end
+      end
+
       # @examples
       #   list :moves                                   # assumes Move
       #   list :moves, :dependent => true               # assumes Move
--- spec/toy/embedded_lists_spec.rb
+++ spec/toy/embedded_lists_spec.rb
@@ -7,6 +7,53 @@ describe Toy::Lists do
     Game.embedded_lists.should == {}
   end
 
+  describe ".parent_reference" do
+    before do
+      Game.embedded_list(:moves)
+    end
+
+    context "with single name" do
+      before do
+        Move.parent_reference(:game)
+        @game = Game.new
+        @move = Move.new
+      end
+
+      it "defines method that calls parent_reference" do
+        @move.parent_reference = @game
+        @move.game.should == @game
+      end
+
+      it "defaults boolean method that checks parent_reference existence" do
+        @move.game?.should be_false
+        @move.parent_reference = @game
+        @move.game?.should be_true
+      end
+    end
+
+    context "with multiple names" do
+      before do
+        Move.parent_reference(:game, :yaypants)
+        @game = Game.new
+        @move = Move.new
+      end
+
+      it "defines method that calls parent_reference" do
+        @move.parent_reference = @game
+        @move.game.should == @game
+        @move.yaypants.should == @game
+      end
+
+      it "defaults boolean method that checks parent_reference existence" do
+        @move.game?.should be_false
+        @move.yaypants?.should be_false
+        @move.parent_reference = @game
+        @move.game?.should be_true
+        @move.yaypants?.should be_true
+      end
+    end
+  end
+
   describe "declaring an embedded list" do
     describe "using conventions" do
       before do
