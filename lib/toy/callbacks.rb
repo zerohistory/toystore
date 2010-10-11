c2929c451c2772ff3d05b81a33f23ea2603bfdf5
Brandon Keepers <brandon@collectiveidea.com>
2010-10-11 17:17:47 -0400

    Wrap instance methods for Callbacks module

--- lib/toy/callbacks.rb
+++ lib/toy/callbacks.rb
@@ -1,27 +1,31 @@
 module Toy
   module Callbacks
     extend ActiveSupport::Concern
+    include ActiveSupport::Callbacks
 
     included do
       extend ActiveModel::Callbacks
       define_model_callbacks :save, :create, :update, :destroy
     end
 
-    def save(*)
-      run_callbacks(:save) { super }
-    end
-
-    def destroy
-      run_callbacks(:destroy) { super }
-    end
-
-    private
-      def create
-        run_callbacks(:create) { super }
+    module InstanceMethods
+      def save(*)
+        run_callbacks(:save) { super }
       end
 
-      def update
-        run_callbacks(:update) { super }
+      def destroy
+        run_callbacks(:destroy) { super }
       end
+
+      private
+
+        def create
+          run_callbacks(:create) { super }
+        end
+
+        def update
+          run_callbacks(:update) { super }
+        end
+    end
   end
 end
